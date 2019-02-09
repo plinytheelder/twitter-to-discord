1ec560ed31d398a7200841c26b9cca6beec79400
plinytheelder <jwjohnson310@gmail.com>
2019-02-08 16:21:55 -0800

    Update tweet.js

--- bin/tweet.js
+++ bin/tweet.js
@@ -135,7 +135,12 @@ module.exports = (tweet, manual) => {
   modifiedText = modifiedText.trim();
 
   // Create a new string to send to Discord
-  let str = `New Tweet from <a href="https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}">${tweet.user.screen_name}</a>`;
+  let str = {
+  "embeds": [{
+    "title": "New Tweet from ${tweet.user.screen_name}:",
+    "url": "https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}"
+  }]
+};
   if (modifiedText) {
     let nameRT;
     if (tweet.retweeted_status) nameRT = tweet.retweeted_status.user.screen_name;
